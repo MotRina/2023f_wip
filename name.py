2454594fe6c877641bb9485a55582f07e1c6552b
ojus <jusrinatin41031@gmail.com>
2023-12-10 13:16:10 +0900

    changeName

--- name.py
+++ name.py
@@ -1,1 +1,8 @@
 # ファイル名の整理
+import os
+directory_path = "./data/"
+
+for root, dirs, files in os.walk(directory_path):
+   for n, file in enumerate(sorted(files), start=1):
+       new_name = os.path.join(root, f"{os.path.basename(root)}_{n}.jpg")
+       os.rename(os.path.join(root, file), new_name)
